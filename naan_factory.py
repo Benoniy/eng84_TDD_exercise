329c0269ec4f21c721b087e04b59062c651f9f51
Ben <benranson5@gmail.com>
2021-03-29 12:26:55 +0100

    added exit clause and loop as well as additional tests

--- naan_factory.py
+++ naan_factory.py
@@ -18,3 +18,16 @@ def run_factory(ingredient1, ingredient2):
     dough = make_dough(ingredient1, ingredient2)
     naan = bake_dough(dough)
     return naan
+
+
+if __name__ == "__main__":
+    while True:
+        user_input = input("Would you like to make naan today?  (y or n)")
+
+        if user_input.lower() == "n":
+            break
+
+        ing1 = input("What is the first ingredient for the dough?: ")
+        ing2 = input("What is the second ingredient for the dough?: ")
+
+        print(f"You made {run_factory(ing1, ing2)}")
--- test_unittest_naan_factory.py
+++ test_unittest_naan_factory.py
@@ -7,10 +7,17 @@ class naanTest(unittest.TestCase):
     def test_make_dough(self):
         self.assertEqual(naan_factory.make_dough("flour", "water"), "dough")
 
-
     def test_bake_dough(self):
         self.assertEqual(naan_factory.bake_dough("dough"), "naan bread")
 
     def test_run_factory(self):
         self.assertEqual(naan_factory.run_factory("flour", "water"), "naan bread")
 
+    def test_make_dough_wrong(self):
+        self.assertEqual(naan_factory.make_dough("water", "salt"), "watery salt")
+
+    def test_bake_dough_wrong(self):
+        self.assertEqual(naan_factory.bake_dough("salt"), "unknown")
+
+    def test_run_factory_wrong(self):
+        self.assertEqual(naan_factory.run_factory("batter", "water"), "unknown")
